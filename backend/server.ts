3aaa400259f5da03cefab2f20344594dd0068388
Bilal Hammi <bilal.hammi@hva.nl>
2025-01-30 11:28:26 +0100

    added tsconfig.json + made structure with files in them.

---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+  
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
