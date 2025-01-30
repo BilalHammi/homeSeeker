2eb24f50b438921c024059fd73dd2b4263ef19fe
Bilal Hammi <bilal.hammi@hva.nl>
2025-01-30 12:43:02 +0100

    changed port number.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4002;
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
 });
